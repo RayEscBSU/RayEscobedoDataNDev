0ce789dfe6206c51fe5efb0c954367db2db3aab0
Raymundo <raymundoescobedo@u.boisestate.edu>
2024-06-19 14:41:55 -0600

    update - added home page animation

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,21 @@ document.addEventListener("DOMContentLoaded", function() {
     // Register the TextPlugin with GSAP
     gsap.registerPlugin(TextPlugin);
 
+    // Set the initial size of the title text
+     gsap.set("#title h1",
+         { fontSize: "5rem",
+         delay:2
+         }
+     );
+
     // Animate the title text
     gsap.to("#title h1",
         {
             duration: 3.5,
             text: "RAY ESCOBEDO DATA & DEVELOPMENT",
             opacity: 1,
-            delay: 2
+            delay: 2,
+            ease: "power2.out" // Ease for a smoother transition
         }
     );
 
